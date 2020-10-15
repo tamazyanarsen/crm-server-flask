5bffe91641a54fa65da97db56fbba196a4fe4425
a.tamazyan <a.tamazyan@focus.bi>
2020-10-15 16:45:59 +0300

    новые фичи

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
+import uuid
+
 import flask
+from flask import request
 from sqlalchemy import Column, Integer, String
 from sqlalchemy import create_engine
-import sqlalchemy
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import bcrypt
 
 engine = create_engine('sqlite:///test.db', echo=True)
 Session = sessionmaker(bind=engine)
@@ -17,15 +20,24 @@ Base = declarative_base()
 
 class User(Base):
     __tablename__ = 'users'
-    id = Column(Integer, primary_key=True)
-    name = Column(String)
-    fullname = Column(String)
-    nickname = Column(String)
+    id = Column(String, primary_key=True)
     email = Column(String)
+    password = Column(String)
+    name = Column(String, nullable=True)
+    fullname = Column(String, nullable=True)
+    nickname = Column(String, nullable=True)
+
+    def __init__(self, email, password, name=None, fullname=None, nickname=None):
+        self.id = str(uuid.uuid4())
+        self.email = email
+        self.password = str(bcrypt.hashpw(password.encode('utf8'), bcrypt.gensalt()))
+        self.name = name
+        self.fullname = fullname
+        self.nickname = nickname
 
     def __repr__(self):
-        return "<User(name='%s', fullname='%s', nickname='%s')>" % (
-            self.name, self.fullname, self.nickname)
+        return "<User(email=%s, name='%s', fullname='%s', nickname='%s')>" % (
+            self.email, self.name, self.fullname, self.nickname)
 
 
 Base.metadata.create_all(engine)
@@ -36,10 +48,34 @@ def index():
     return flask.jsonify({'success': True})
 
 
+@app.route('/users/login', methods=['POST'])
+def login():
+    login = request.form['login']
+    password = request.form['password']
+    session = Session()
+    user = session.query(User).filter(User.email == login).filter(
+        bcrypt.checkpw(password.encode('utf8'), User.password)).first()
+    session.close()
+    return flask.jsonify(user.id)
+
+
+@app.route('/users/add', methods=['POST'])
+def sign_up():
+    email = request.form['email']
+    password = request.form['password']
+    user = User(email, password)
+    session = Session()
+    session.add(user)
+    session.commit()
+    session.close()
+    return flask.jsonify(repr(user))
+
+
 @app.route('/users')
 def get_all_users():
     session = Session()
-    data = session.query(User).all()
+    data = list(map(repr, session.query(User).all()))
+    print(data)
     session.close()
     return flask.jsonify(data)
 
