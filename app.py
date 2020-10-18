ca7106869f81b86cb1aa4903279af0e852870f6d
a.tamazyan <tamazyanarsen@mail.ru>
2020-10-18 18:09:00 +0300

    исправления

--- app.py
+++ app.py
@@ -8,7 +8,8 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import bcrypt
 import flask_bcrypt
-from flask_jwt_extended import JWTManager, create_access_token, get_jwt_identity
+from flask_jwt_extended import JWTManager, create_access_token, get_jwt_identity, create_refresh_token, \
+    jwt_refresh_token_required, set_access_cookies, unset_jwt_cookies, jwt_required, set_refresh_cookies
 from flask_sqlalchemy import SQLAlchemy
 import datetime
 
@@ -18,6 +19,11 @@ app = flask.Flask(__name__)
 app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 app.config['JWT_SECRET_KEY'] = str(datetime.datetime.now().timestamp())
+app.config['JWT_TOKEN_LOCATION'] = ['cookies']
+app.config['JWT_COOKIE_SECURE'] = False
+app.config['JWT_ACCESS_COOKIE_PATH'] = '/api/'
+app.config['JWT_REFRESH_COOKIE_PATH'] = '/token/refresh'
+# app.config['JWT_COOKIE_CSRF_PROTECT'] = True
 
 jwt = JWTManager(app)
 db = SQLAlchemy(app)
@@ -41,13 +47,13 @@ class User(db.Model):
         self.nickname = nickname
 
     def __repr__(self):
-        return "<User(email=%s, name='%s', fullname='%s', nickname='%s')>" % (
-            self.email, self.name, self.fullname, self.nickname)
+        return f'User (email={self.email}, password={self.password})'
 
 
 class Token(db.Model):
     __tablename__ = 'tokens'
     id = db.Column(db.String, primary_key=True)
+    user_id = db.Column(db.String)
     token = db.Column(db.String)
     created_at = db.Column(db.String)
     token_live = db.Column(db.String)  # время жизни токена задаем в минутах
@@ -72,15 +78,20 @@ def login():
     print(request, request.json)  # работает, если в Content-Type передавать application/json
     email = request.form['email']
     password = request.form['password']
-    print(bcrypt.hashpw(password.encode('utf8'), bcrypt.gensalt()))
-    user = User.query.filter(User.email == email).filter(
-        bcrypt.hashpw(password.encode('utf8'), bcrypt.gensalt()) == User.password).first()
-    if user:
-        access_token = create_access_token(identity=user.id)
-        answer = jsonify(access_token=access_token), 200
+    user = User.query.filter(User.email == email).filter(User.email == email).first()
+    print(f'user: {user}')
+    if bcrypt.checkpw(password.encode('utf8'), user.password.encode('utf8')):
+        access_token = create_access_token(identity=email)
+        refresh_token = create_refresh_token(identity=email)
+
+        # Set the JWTs and the CSRF double submit protection cookies
+        # in this response
+        resp = jsonify({'login': True}), 200
+        set_access_cookies(resp, access_token)
+        set_refresh_cookies(resp, refresh_token)
     else:
-        answer = jsonify({'error': True, 'code': 400}), 400
-    return answer
+        resp = jsonify({'login': False}), 400
+    return resp
 
 
 @app.route('/users/<user_id>')
@@ -95,8 +106,36 @@ def create():
     user = User(email, password)
     db.session.add(user)
     db.session.commit()
-    answer = jsonify(access_token=create_access_token(identity=user.id)), 200
+    answer = jsonify(access_token=create_access_token(identity=user.email)), 200
     return answer
+
+
+@app.route('/token/refresh', methods=['POST'])
+@jwt_refresh_token_required
+def refresh():
+    # Create the new access token
+    current_user = get_jwt_identity()
+    access_token = create_access_token(identity=current_user)
+
+    # Set the access JWT and CSRF double submit protection cookies
+    # in this response
+    resp = jsonify({'refresh': True})
+    set_access_cookies(resp, access_token)
+    return resp, 200
+
+
+@app.route('/token/remove', methods=['POST'])
+def logout():
+    resp = jsonify({'logout': True})
+    unset_jwt_cookies(resp)
+    return resp, 200
+
+
+@app.route('/api/example', methods=['GET'])
+@jwt_required
+def protected():
+    username = get_jwt_identity()
+    return jsonify({'hello': 'from {}'.format(username)}), 200
 
 
 @app.route('/users')
