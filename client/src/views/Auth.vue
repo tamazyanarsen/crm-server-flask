4dc953c67efc5778f1cfe94a614f84bafacbc445
a.tamazyan <tamazyanarsen@focus.bi>
2020-10-21 16:29:01 +0300

    Форма авторизации

--- client/src/views/Auth.vue
+++ client/src/views/Auth.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    <v-form action="">
+  <div class="auth__block">
+    <v-form>
       <v-text-field label="Логин" placeholder="Введите логин"></v-text-field>
       <v-text-field label="Пароль" placeholder="Введите пароль" type="password"></v-text-field>
     </v-form>
@@ -13,6 +13,10 @@ export default {
 }
 </script>
 
-<style scoped>
-
+<style scoped lang="scss">
+.auth__block {
+  height: auto;
+  max-width: 60%;
+  margin: 20% auto auto;
+}
 </style>
